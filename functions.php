9a87312aec99b84007301379e367c6be3384cfe3
gaia77 <J@kass77>
2021-02-12 13:07:30 -0800

    menu set up

--- functions.php
+++ functions.php
@@ -50,7 +50,7 @@ if ( ! function_exists( 'gsportfolio_theme_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus(
 			array(
-				'menu-1' => esc_html__( 'Primary', 'gsportfolio-theme' ),
+				'header' => esc_html__( 'Header Menu Location', 'gsportfolio-theme' ),
 			)
 		);
 
